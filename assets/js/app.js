f9e3ded9f273eb82ad01b39f730fd462ab2c3499
Elias Melaku <se.Elias.Melaku@gmail.com>
2021-02-25 18:40:58 +0300

    fixed another issue that prevneted the quiz from loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,24 +3,18 @@ const easy = document.querySelector(".easy");
 const normal = document.querySelector(".normal");
 const hard = document.querySelector(".hard");
 
-const username = document.querySelector("#username");
-const form = document.querySelector("#usernameForm");
+const urlParams = new URLSearchParams(window.location.search);
+var username = String(urlParams.get("username"));
 
-// get the username passed from the prev page
-
-if (window.location.href == "index.html") {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    window.location.href = `difficulty.html?username=${username.value}`;
-  });
+if (username == null) {
+  window.location.href = "index.html";
 }
-
 easy.addEventListener("click", () => {
-  window.location.href = `quiz.html?difficulty=easy`;
+  window.location.href = `quiz.html?username=${username}&difficulty=easy`;
 });
 normal.addEventListener("click", () => {
-  window.location.href = `quiz.html?difficulty=medium`;
+  window.location.href = `quiz.html?username=${username}&difficulty=medium`;
 });
 hard.addEventListener("click", () => {
-  window.location.href = `quiz.html?difficulty=hard`;
+  window.location.href = `quiz.html?username=${username}&difficulty=hard`;
 });
