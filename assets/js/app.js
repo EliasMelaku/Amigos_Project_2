0f4d66adfdedb515ca99a25e51a753d729f8a2d5
Elias Melaku <se.Elias.Melaku@gmail.com>
2021-02-25 18:25:35 +0300

    fixed minor bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,16 @@ const normal = document.querySelector(".normal");
 const hard = document.querySelector(".hard");
 
 const username = document.querySelector("#username");
-const form = document.querySelector(".usernameForm");
+const form = document.querySelector("#usernameForm");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  window.location.href = `difficulty.html?username=${username.value}`;
-});
+// get the username passed from the prev page
+
+if (window.location.href == "index.html") {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    window.location.href = `difficulty.html?username=${username.value}`;
+  });
+}
 
 easy.addEventListener("click", () => {
   window.location.href = `quiz.html?difficulty=easy`;
