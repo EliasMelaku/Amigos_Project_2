31de9f678ab569af18cd9c9aec413a8b2a4469fd
Elias Melaku <se.Elias.Melaku@gmail.com>
2021-02-25 18:10:49 +0300

    added form to accept username

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
+// get UI elements
 const easy = document.querySelector(".easy");
 const normal = document.querySelector(".normal");
 const hard = document.querySelector(".hard");
+
+const username = document.querySelector("#username");
+const form = document.querySelector(".usernameForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  window.location.href = `difficulty.html?username=${username.value}`;
+});
 
 easy.addEventListener("click", () => {
   window.location.href = `quiz.html?difficulty=easy`;
